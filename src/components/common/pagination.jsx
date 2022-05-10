14659f28cb35d36aca4fee05fb72c3ac2a65d396
Amir <mamyrbas@gmail.com>
2022-05-10 19:59:52 +0430

    Pagination Added

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import _ from "lodash";
+import propTypes from "prop-types";
+
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+
+  if (pagesCount === 1) return null;
+
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="flex gap-2 justify-center">
+        {pages.map((page) => (
+          <li key={page}>
+            <button
+              className={`py-2 px-3 rounded-lg ${
+                page === currentPage ? "bg-[#320307] text-white" : ""
+              }`}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  itemsCount: propTypes.number.isRequired,
+  pageSize: propTypes.number.isRequired,
+  currentPage: propTypes.number.isRequired,
+  onPageChange: propTypes.func.isRequired,
+};
+
+export default Pagination;
