bd656fe6d9ff45cba37ec7b79db7d7409a2b433c
Amir <mamyrbas@gmail.com>
2022-05-11 17:15:57 +0430

    shop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { React, Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
-import Index from './components/pages/Index';
+import Index from "./components/pages/Index";
+import Shop from "./components/pages/Shop";
 
 function App() {
   return (
     <Fragment>
-
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/">
+          <Route index element={<Index />} />
+          <Route path="shop">
+            <Route index element={<Shop />} />
+          </Route>
+        </Route>
       </Routes>
-
     </Fragment>
   );
 }
