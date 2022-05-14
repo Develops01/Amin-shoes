4d972795d1f29141548aabce822bbd200680cb31
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-14 12:35:23 +0430

    user reducer added

--- src/store/entities/index.js
+++ src/store/entities/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import products from "./products";
 import modal from "./modal";
+import user from './user';
 
 export default combineReducers({
     products,
     modal,
+    user
 });
