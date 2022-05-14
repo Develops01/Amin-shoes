37fec2fd25ad6715ddddb36dd26c2b8a7b400723
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-14 13:44:49 +0430

    using Link component, adding shop and login link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,36 +1,50 @@
 import React from "react";
 
-import shosePic from "../img/shose.png";
+import shoesPic from "../img/shoes.png";
+import { Link } from "react-router-dom";
+import { setCurrentModal } from "../store/entities/modal";
+import { useDispatch } from "react-redux";
 
 function Nav() {
+  const dispatch = useDispatch();
+
   return (
     <nav>
       <ul className="container mx-auto flex flex-col sm:flex-row gap-5 items-center justify-center bg-[#320307] text-white py-5 sm:rounded-full">
         <li>
-          <a href="">مردانه</a>
+          <Link
+            to=""
+            onClick={() => {
+              dispatch(setCurrentModal("LOGIN"));
+            }}
+          >
+            ورود
+          </Link>
         </li>
         <li>
-          <a href="">مردانه</a>
+          <Link to="/shop">فروشگاه</Link>
         </li>
         <li>
-          <a href="">مردانه</a>
+          <Link to="">مردانه</Link>
         </li>
 
         <div className="relative w-56 hidden sm:block">
           <div className="border-2 border-[#9B6F47] bg-white w-56 absolute sm:-translate-y-1/2 flex flex-col items-center justify-center p-4">
-            <img src={shosePic} alt="" className="mb-3" />
-            <span className="text-[#3E1304] text-3xl font-extrabold">کفش امیر</span>
+            <img src={shoesPic} alt="" className="mb-3" />
+            <span className="text-[#3E1304] text-3xl font-extrabold">
+              کفش امیر
+            </span>
           </div>
         </div>
 
         <li>
-          <a href="">مردانه</a>
+          <Link to="">مردانه</Link>
         </li>
         <li>
-          <a href="">مردانه</a>
+          <Link to="">مردانه</Link>
         </li>
         <li>
-          <a href="">مردانه</a>
+          <Link to="">مردانه</Link>
         </li>
       </ul>
     </nav>
