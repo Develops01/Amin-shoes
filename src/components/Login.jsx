ec036f5eea05922130e5d1791e2d9aa808e97c0d
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-14 12:31:20 +0430

    handleSubmit completed.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
 import ModalWrapper from "./common/ModalWrapper";
+import { useDispatch } from "react-redux";
+import { login } from "./../store/entities/user";
+import { toast } from "react-toastify";
+import { closeModal } from './../store/entities/modal';
 
 function Login(props) {
+  const dispatch = useDispatch();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
+    dispatch(
+      login({
+        email,
+        password,
+        onSuccess,
+        onError,
+      })
+    );
+
+    function onError() {
+      toast.error("خطایی رخ داده است");
+    }
+    function onSuccess() {
+      toast.info("ورود موفقیت آمیز بود");
+      dispatch(closeModal());
+    }
   }
 
   return (
@@ -16,14 +38,14 @@ function Login(props) {
         <input
           type="email"
           value={email}
-          onChange={setEmail}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="ایمیل"
           className="py-2 px-4 outline-0 border-2 border-gray-300 rounded-lg my-1"
         />
         <input
           type="password"
           value={password}
-          onChange={setPassword}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="رمز"
           className="py-2 px-4 outline-0 border-2 border-gray-300 rounded-lg my-1"
         />
@@ -31,7 +53,7 @@ function Login(props) {
           className="py-2 px-4 outline-0 bg-orange-500 text-white rounded-lg my-1 cursor-pointer"
           onClick={handleSubmit}
         >
-           ورود
+          ورود
         </button>
       </div>
     </ModalWrapper>
