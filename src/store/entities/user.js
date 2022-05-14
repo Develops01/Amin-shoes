55d232e179dd2ab69fc4a5ca2fa7b7c23ed7f4c9
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-14 12:38:33 +0430

    login, loginWithJWT, logout functionality Added

--- src/store/entities/user.js
+++ src/store/entities/user.js
@@ -1,26 +1,112 @@
+import { createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode";
+import http from "../../services/httpService";
+import { apiCallBegan } from "./../apiActions";
 
-import { createSlice } from '@reduxjs/toolkit';
-import { jwtDecode } from 'jwt-decode';
-
-let lastId = 0;
+const apiEndpoint = process.env.REACT_APP_API_LOGIN;
+const tokenKey = "token";
 
 const slice = createSlice({
-    name: "user",
-    initialState: {
-        jwt: null,
-        iat: null,
-        exp: null,
-    },
-    reducers: {
-        loggedInWithJWT: (state, action) => {
-            state.jwt = action.payload;
+  name: "user",
+  initialState: {
+    token: null,
+    name: null,
+    _id: null,
+    iat: null,
+    exp: null,
+  },
+  reducers: {
+    loggedIn: (state, { payload }) => {
+      const token = payload.data.token;
 
-            const decoded = jwtDecode(action.payload);
-            state.iat = decoded.iat;
-            state.exp = decoded.exp;
-        },
-    }
+      localStorage.setItem(tokenKey, token);
+
+      const decoded = jwtDecode(token);
+      Object.keys(state).forEach((key) => {
+        state[key] = decoded[key];
+      });
+
+      state.token = token;
+    },
+    loggedInWithJWT: (state, { payload: token }) => {
+      localStorage.setItem(tokenKey, token);
+
+      const decoded = jwtDecode(token);
+      Object.keys(state).forEach((key) => {
+        state[key] = decoded[key];
+      });
+
+      state.token = token;
+    },
+    loggedOut: (state) => {
+      localStorage.removeItem(tokenKey);
+      Object.keys(state).forEach((key) => {
+        state[key] = null;
+      });
+    },
+    jwtHasSet: (state) => {
+      const token = localStorage.getItem(tokenKey) || null;
+      if (!token) return;
+
+      const decoded = jwtDecode(token);
+
+      Object.keys(state).forEach((key) => {
+        state[key] = decoded[key];
+      });
+
+      state.token = token;
+    },
+  },
 });
 
-export const { addUser, assignBug } = slice.actions;
 export default slice.reducer;
+
+const { loggedIn, loggedInWithJWT, loggedOut, jwtHasSet } = slice.actions;
+
+// ACTIONS
+export const setJwt = () => (dispatch) => {
+  return dispatch({
+    type: jwtHasSet.type,
+  });
+};
+
+export const login =
+  ({ email, password, onSuccess, onError }) =>
+  async (dispatch) => {
+    return dispatch(
+      apiCallBegan({
+        url: apiEndpoint,
+        method: "post",
+        data: {
+          email,
+          pass: password,
+        },
+        onSuccess: onSucceed,
+        onError,
+      })
+    );
+
+    function onSucceed(data) {
+      dispatch({ type: loggedIn.type, payload: data });
+      if (onSuccess) onSuccess();
+    }
+  };
+
+export const loginWithJWT = (jwt) => (dispatch) => {
+  return dispatch({
+    type: loggedInWithJWT.type,
+    payload: {
+      jwt,
+    },
+  });
+};
+
+export const logout = () => (dispatch) => {
+  return dispatch({
+    type: loggedOut.type,
+  });
+};
+
+// SELECTORS
+export const selectCurrentUser = (state) => state.entities.user;
+export const selectJwt = (state) => state.entities.user.jwt;
