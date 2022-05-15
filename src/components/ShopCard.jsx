18412d32d0c2a6a88ea921b73fac15f747f174cd
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-15 16:36:46 +0430

    add to cart added

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addProductToCart } from "../store/entities/products";
 
-function ShopCard({ title, image, price, link }) {
+import shopPic from "../img/shop.svg";
+
+function ShopCard({ title, image, price, link, id }) {
+  const dispatch = useDispatch();
+
   return (
-    <article>
+    <article className="relative">
+      <button className="absolute top-0 left-0 m-2 p-2 rounded-full hover:bg-gray-100" onClick={()=>dispatch(addProductToCart(id))}>
+        <img src={shopPic} alt="" />
+      </button>
       <Link to={link} className="flex flex-col p-4 border-2 text-center">
         <img src={image} alt="" className="mb-4" />
         <h3 className="text-sm font-extrabold">{title}</h3>
