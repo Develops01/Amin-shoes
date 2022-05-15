d713b0635def7c0330f99046c7508202c390fcf5
Amir <60928821+Amir-A-M@users.noreply.github.com>
2022-05-15 18:51:19 +0430

    pagination added

--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useStore } from "react-redux";
-import { selectProducts, loadProducts, selectProductsLoading } from "./../store/entities/products";
+import { selectProducts, loadProducts, selectProductsLoading, selectTotalPages } from "./../store/entities/products";
 import ShopCard from "./ShopCard";
 import ShopCardTwo from "./ShopCardTwo";
 import Loading from "./common/Loading";
-
+import Pagination from "./common/pagination";
 import ShopListFilters from './ShopListFilters';
 
 function ShopList({baseProductLinkUrl = ""}) {
   const [filtered, setFiltered] = useState([]);
   const [listStyle, setListStyle] = useState("card");
   const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pagesCount = useSelector((state) => selectTotalPages(state));
 
   const productList = useSelector((state) => selectProducts(state));
   const loading = useSelector((state) => selectProductsLoading(state));
@@ -19,15 +21,22 @@ function ShopList({baseProductLinkUrl = ""}) {
   const imgBaseUrl = process.env.REACT_APP_BASE_IMAGE_URL;
 
   useEffect(() => {
-    store.dispatch(loadProducts({page: 1, limit: process.env.REACT_APP_API_PRODUCTS_LIMIT}));
+    store.dispatch(loadProducts());
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Filters
+  //      ==== FILTERS ====
   useEffect(() => {
     setFiltered(productList);
   }, [productList]);
 
+  // Paginate
+  useEffect(() => {
+    store.dispatch(loadProducts({ page: currentPage }));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  // Search
   useEffect(() => {
     setFiltered(productList.filter((p) => p.name.includes(searchQuery)));
   }, [productList, searchQuery]);
@@ -70,8 +79,14 @@ function ShopList({baseProductLinkUrl = ""}) {
           )
         )}
       </div>
+
+      <Pagination pagesCount={pagesCount} currentPage={currentPage} onPageChange={handlePageChange} />
     </section>
   );
+
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
 }
 
 export default ShopList;
